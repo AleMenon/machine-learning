af3d6dc0efc33ce3ee71fc46a656145ba0ca38d4
Gabriel Rodrigues Estefanes <estefanesgabriel@gmail.com>
2025-10-24 22:33:21 -0300

    Implementação Random Forest - Aprendizado de Máquina

---
+++ random-forest/main.py
@@ -0,0 +1,105 @@
+from sklearn.model_selection import train_test_split
+import numpy as np
+import pandas as pd
+import graphviz
+from RandomForest import RandomForest
+
+np.random.seed(1234)
+
+PATH = "C:/Gabriel/UTFPR/Sistemas Inteligentes/Machine Learning/machine-learning/datasets/treino_sinais_vitais_com_label.csv"
+
+def add_nodes_edges(dot, node, feature_names, node_id=0):
+    
+    if node.is_leaf_node():
+        dot.node(str(node_id), label=f"Valor: {node.value}", shape="box")
+        return node_id + 1
+    
+    feature_name = feature_names[node.feature]
+    label = f"{feature_name} <= {node.threshold:.2f}"
+    dot.node(str(node_id), label=label, shape="ellipse")
+    
+    left_child_id = node_id + 1
+    
+    next_id = add_nodes_edges(dot, node.left, feature_names, left_child_id)
+    dot.edge(str(node_id), str(left_child_id), label="True")
+    
+    right_child_id = next_id
+    
+    final_id = add_nodes_edges(dot, node.right, feature_names, right_child_id)
+    dot.edge(str(node_id), str(right_child_id), label="False")
+    
+    return final_id
+
+def plot_tree_graphviz(tree, feature_names, filename="decision_tree_depth10"):
+    
+    dot = graphviz.Digraph(comment='Decision Tree', format='svg')
+    
+    dot.attr(size='150,150') 
+    
+    dot.attr('node', fontsize='8')
+    dot.attr('edge', fontsize='8')
+
+    dot.attr(ranksep='1.0', nodesep='0.5')
+    
+    add_nodes_edges(dot, tree.root, feature_names)
+    
+    try:
+        dot.render(filename, view=True)
+        print(f"\nÁrvore salva como '{filename}.svg'.")
+        
+    except Exception as e:
+        print(f"\nErro ao gerar a imagem da árvore: {e}")
+
+def print_tree(node, feature_names, depth=0):
+    
+    indent = "  " * depth
+
+    if node.is_leaf_node():
+        print(f"{indent}Prever Valor: {node.value}")
+        return
+
+    feature_name = feature_names[node.feature]
+    
+    print(f"{indent}Condição: {feature_name} <= {node.threshold:.2f}")
+
+    print(f"{indent}--> True (Esquerda):")
+    print_tree(node.left, feature_names, depth + 1)
+
+    print(f"{indent}--> False (Direita):")
+    print_tree(node.right, feature_names, depth + 1)
+
+def accuracy(y_true, y_pred):
+        accuracy = np.sum(y_true == y_pred) / len(y_true)
+        return accuracy
+
+if __name__ == "__main__":
+    dataset = pd.read_csv(PATH)
+    dataset = dataset.drop(columns=["id", "p_sist", "p_diast", "gravidade"])
+
+    X = dataset.drop("classe", axis=1)
+    y = dataset["classe"]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=1234
+    )
+
+    clf = RandomForest(n_trees=20)
+    clf.fit(X_train.values, y_train.values)
+    predictions = clf.predict(X_test.values)
+
+    acc = accuracy(y_test, predictions)
+    print(acc)
+
+    feature_names = X.columns.tolist() 
+
+    if clf.trees:
+        print("\n--- Estrutura da Árvore da Floresta ---")
+        
+        arvore_para_ver = clf.trees[0] 
+        
+        print_tree(arvore_para_ver.root, feature_names)
+        
+        plot_tree_graphviz(arvore_para_ver, feature_names, filename="random_forest_arvore_0")
+        
+    else:
+        print("A floresta não foi treinada ou não contém árvores.")
